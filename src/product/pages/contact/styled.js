f98c2c5d2920b09cbfbdd09432b1668842f2f1a4
Ulisses Rosa <ulisses.skysect@hotmail.com>
2022-05-13 20:52:00 -0300

    Home and Contact Page

---
+++ src/product/pages/contact/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+export const ContactArea = styled.div`
+    display: flex;
+    width:flex;
+    justify-content: center;
+    align-items: center;
+    border:  5px solid burlywood;
+    border-radius: 5px;
+    border-color: burlywood;
+    border-width: 10px 10px 10px 10px;
+    margin-right: 15%;
+    margin-left: 15%;
+    background: bisque;
+    input
+`;
+
+export const InputText = styled.input`
+  width: 85%;
+  display: inline-block;
+`;
+
+export const TextArea = styled.textarea`
+  width: 85%;
+  display: inline-block;
+`;
+
+export const Organizer = styled.div`
+  justify-content: center;
+  align-items: center;
+  display: inline-block;
+`;
