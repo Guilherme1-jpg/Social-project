c25fa778817ad9aad9a062188c1dfa168fca0ec4
Ulisses Rosa <ulisses.skysect@hotmail.com>
2022-05-22 22:31:46 -0300

    Remove unnecessary show more texts

--- src/product/pages/prey/index.js
+++ src/product/pages/prey/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import PgFacebook from "../../../components/pgFacebook";
+import { Divider } from "@mui/material";
 import ShowMoreText from "react-show-more-text";
 
 import PreyImage from "../../../_assets/img/prey/preyImage.png";
@@ -19,10 +19,13 @@ const Prey = () => {
       <div class="container">
         <div class="col-12">
           <div class="row">
-            <h1 class="mb-5 text-center font-italic">Oração</h1>
+            <h1 class="mb-5 text-center font-italic">
+              <Divider>Oração</Divider>
+            </h1>
             <figure>
               <img alt="" src={PreyImage} class="img-fluid" align="center" />
             </figure>
+            <Divider />
             <br />
             <div class="row mb-12">
               <div class="col-12  d-flex justify-content-end"></div>
@@ -46,17 +49,24 @@ const Prey = () => {
                   <p class="text-justify">
                     Portanto a Vida da Monja, é marcada pela Oração, pelo
                     diálogo com Deus, que acontece em vários momentos do dia, em
-                    Comunidade e individualmente :<br /><br />
+                    Comunidade e individualmente :<br />
+                    <br />
                     <div>
-                      <strong><p>• Oficio Divino</p></strong>
+                      <strong>
+                        <p>• Oficio Divino</p>
+                      </strong>
                       <br />
                     </div>
                     <div>
-                    <strong><p>• Meditação da Palavra</p></strong>
+                      <strong>
+                        <p>• Meditação da Palavra</p>
+                      </strong>
                       <br />
                     </div>
                     <div>
-                    <strong><p>• Santa Missa</p></strong>
+                      <strong>
+                        <p>• Santa Missa</p>
+                      </strong>
                       <br />
                     </div>
                     <div>
@@ -74,151 +84,163 @@ const Prey = () => {
                 </ShowMoreText>
               </div>
             </div>
+            <Divider />
 
             <div class="row mb-3">
               <div class="col-5 d-flex justify-content-end">
                 <figure class="my-4">
-                  <img src={PreyImage2} alt="" width= "350px" height="350px" class="img-fluid" />
+                  <img
+                    src={PreyImage2}
+                    alt=""
+                    width="350px"
+                    height="350px"
+                    class="img-fluid"
+                  />
                 </figure>
               </div>
               <div class="col-7">
-                <h4>Ofício Divino</h4>
-                <ShowMoreText
-                  lines={6}
-                  more={"Mostrar mais"}
-                  less={"Mostrar menos"}
-                  onClick={onClick}
-                  expanded={expand}
-                >
-                  <p class="text-justify">
-                    Para estimular e nutrir essa Presença do Senhor, as Monjas
-                    são chamadas ao Louvor ao Criador desde o nascer ao pôr do
-                    sol e esta Oração Litúrgica, não o fazem por si mesmas, mas
-                    em nome de toda a Igreja e pela humanidade. Por isso, elevam
-                    salmos e hinos de louvor e gratidão ao Senhor na Liturgia
-                    das Horas, 7 vezes durante o dia, comunitariamente.
-                    Portanto o Mosteiro é um recanto de Oração, é o Lugar da
-                    Casa de Deus e oferece um ambiente propício, para todos os
-                    fiéis freqüentadores, que desejam crescer na oração pessoal,
-                    na meditação da Palavra e ficar aos pés do Senhor , no
-                    Santíssimo Sacramento como verdadeiros adoradores.
-                  </p>
-                </ShowMoreText>
+                <h4>
+                  <Divider>Ofício Divino</Divider>
+                </h4>
+                <p class="text-justify">
+                  Para estimular e nutrir essa Presença do Senhor, as Monjas são
+                  chamadas ao Louvor ao Criador desde o nascer ao pôr do sol e
+                  esta Oração Litúrgica, não o fazem por si mesmas, mas em nome
+                  de toda a Igreja e pela humanidade. Por isso, elevam salmos e
+                  hinos de louvor e gratidão ao Senhor na Liturgia das Horas, 7
+                  vezes durante o dia, comunitariamente. Portanto o Mosteiro é
+                  um recanto de Oração, é o Lugar da Casa de Deus e oferece um
+                  ambiente propício, para todos os fiéis freqüentadores, que
+                  desejam crescer na oração pessoal, na meditação da Palavra e
+                  ficar aos pés do Senhor , no Santíssimo Sacramento como
+                  verdadeiros adoradores.
+                </p>
               </div>
             </div>
+            <Divider />
 
             <div class="row mb-3">
               <div class="col-5 d-flex justify-content-end">
                 <figure class="my-4">
-                  <img src={PreyImage3} alt="" width= "350px" height="350px" class="img-fluid" />
+                  <img
+                    src={PreyImage3}
+                    alt=""
+                    width="350px"
+                    height="350px"
+                    class="img-fluid"
+                  />
                 </figure>
               </div>
               <div class="col-7" justify-content="left">
-                <h4>Na Corte de Castela</h4>
-                <ShowMoreText
-                  lines={15}
-                  more={"Mostrar mais"}
-                  less={"Mostrar menos"}
-                  onClick={onClick}
-                  expanded={expand}
-                >
-                  <p class="text-justify">
-                    Os Monges e Monjas, desde suas origens, buscam viver este
-                    dom da Oração,iluminados pela Luz das Sagradas Escrituras. A
-                    meditação assídua da Palavra, é a chave para o
-                    aprofundamento e conhecimento do Senhor e da realização de
-                    Sua Vontade em nossa vida.Esta meditação a monja aprende a
-                    realizar em 4 etapas:
-                    <br />
-                    <br />
-                    Lectio: Leitura e aprofundamento da Palavra
-                    <br />
-                    <br />
-                    Meditatio: Meditação da Palavra, a Monja confronta o texto
-                    Sagrado com sua vida
-                    <br />
-                    <br />
-                    Oratio: Iluminada pela Palavra, pede a Graça do Senhor para
-                    a sua transformação e conversão.
-                    <br />
-                    <br />
-                    Contemplatio: Com o coração humilde e feliz, a Consagrada se
-                    deixa amar pelo Senhor e o ama na plenitude de sua entrega,
-                    sendo dócil aos apelos do Senhor.
-                  </p>
-                  <p class="text-justify">
-                    <em>
-                      <strong>
-                        É na Oração e meditação, que a Monja encontra força,
-                        para transformar sua vida , à Vida de Cristo, fazendo
-                        dela uma nova criatura, liberta de tudo aquilo que a
-                        impede de ser livre na Vida da Graça, à Luz da Fé.
-                      </strong>
-                    </em>
-                  </p>
-                </ShowMoreText>
-              </div>
-            </div>
+                <h4>
+                  <Divider>Meditação da Palavra</Divider>
+                </h4>
 
-            <div class="row mb-3">
-              <div class="col-5 d-flex justify-content-end">
-                <figure class="my-4">
-                  <img src={PreyImage4} alt="" width= "350px" height="350px" class="img-fluid" />
-                </figure>
-              </div>
-              <div class="col-7" justify-content="left">
-                <h4>Santa Missa</h4>
-                <ShowMoreText
-                  lines={8}
-                  more={"Mostrar mais"}
-                  less={"Mostrar menos"}
-                  onClick={onClick}
-                  expanded={expand}
-                >
-                  <p>
-                    Na Eucaristia, o Sacrifício de Cristo se torna também o
-                    sacrifício dos membros de seu Corpo Místico. A vida dos
-                    Consagrados , com tudo aquilo que realizam: seus louvores,
-                    suas orações, seus trabalhos e sofrimentos, são unidos aos
-                    de Cristo e à sua Oferenda Total, e adquirem assim um valor
-                    novo.
-                  </p>
-                  <p>
-                    A Consagração da Religiosa ao Senhor é uma oferenda
-                    permanente, que diariamente com Jesus, no momento do Santo
-                    Sacrifício da Missa, a Monja se oferece com Jesus ao Pai e
-                    em uma só oferenda, pela salvação do mundo. As Monjas unidas
-                    a Cristo, são como a gota de água que se adiciona ao Cálice
-                    com Vinho e se deixam diluir na Essência de Cristo, na
-                    Unidade do Espírito Santo e em Louvor ao Pai.
-                  </p>
-                  <p>
+                <p class="text-justify">
+                  Os Monges e Monjas, desde suas origens, buscam viver este dom
+                  da Oração,iluminados pela Luz das Sagradas Escrituras. A
+                  meditação assídua da Palavra, é a chave para o aprofundamento
+                  e conhecimento do Senhor e da realização de Sua Vontade em
+                  nossa vida.Esta meditação a monja aprende a realizar em 4
+                  etapas:
+                  <br />
+                  <br />
+                  Lectio: Leitura e aprofundamento da Palavra
+                  <br />
+                  <br />
+                  Meditatio: Meditação da Palavra, a Monja confronta o texto
+                  Sagrado com sua vida
+                  <br />
+                  <br />
+                  Oratio: Iluminada pela Palavra, pede a Graça do Senhor para a
+                  sua transformação e conversão.
+                  <br />
+                  <br />
+                  Contemplatio: Com o coração humilde e feliz, a Consagrada se
+                  deixa amar pelo Senhor e o ama na plenitude de sua entrega,
+                  sendo dócil aos apelos do Senhor.
+                </p>
+                <p class="text-justify">
+                  <em>
                     <strong>
-                      A Monja Concepcionista Franciscana, faz de sua vida uma
-                      Oração de entrega ao Senhor:
+                      É na Oração e meditação, que a Monja encontra força, para
+                      transformar sua vida , à Vida de Cristo, fazendo dela uma
+                      nova criatura, liberta de tudo aquilo que a impede de ser
+                      livre na Vida da Graça, à Luz da Fé.
                     </strong>
-                  </p>
-
-                  <p>
-                    <em>
-                      “ ... A entrada nesta Ordem é uma singular oferta, que a
-                      Nosso Redentor e a Sua Gloriosa Mãe se oferece, dando-se a
-                      Ele, com corpo e alma em hóstia viva ” Santa Regra II
-                    </em>
-                  </p>
-                  <p></p>
-                </ShowMoreText>
+                  </em>
+                </p>
               </div>
             </div>
+            <Divider />
 
             <div class="row mb-3">
               <div class="col-5 d-flex justify-content-end">
                 <figure class="my-4">
-                  <img src={PreyImage5} alt="" width= "350px" height="350px" class="img-fluid" />
+                  <img
+                    src={PreyImage4}
+                    alt=""
+                    width="350px"
+                    height="350px"
+                    class="img-fluid"
+                  />
                 </figure>
               </div>
               <div class="col-7" justify-content="left">
-                <h4>No Coração da Igreja: Somos Intercessoras</h4>
+                <h4>
+                  <Divider>Santa Missa</Divider>
+                </h4>
+                <p>
+                  Na Eucaristia, o Sacrifício de Cristo se torna também o
+                  sacrifício dos membros de seu Corpo Místico. A vida dos
+                  Consagrados , com tudo aquilo que realizam: seus louvores,
+                  suas orações, seus trabalhos e sofrimentos, são unidos aos de
+                  Cristo e à sua Oferenda Total, e adquirem assim um valor novo.
+                </p>
+                <p>
+                  A Consagração da Religiosa ao Senhor é uma oferenda
+                  permanente, que diariamente com Jesus, no momento do Santo
+                  Sacrifício da Missa, a Monja se oferece com Jesus ao Pai e em
+                  uma só oferenda, pela salvação do mundo. As Monjas unidas a
+                  Cristo, são como a gota de água que se adiciona ao Cálice com
+                  Vinho e se deixam diluir na Essência de Cristo, na Unidade do
+                  Espírito Santo e em Louvor ao Pai.
+                </p>
+                <p>
+                  <strong>
+                    A Monja Concepcionista Franciscana, faz de sua vida uma
+                    Oração de entrega ao Senhor:
+                  </strong>
+                </p>
+
+                <p>
+                  <em>
+                    “ ... A entrada nesta Ordem é uma singular oferta, que a
+                    Nosso Redentor e a Sua Gloriosa Mãe se oferece, dando-se a
+                    Ele, com corpo e alma em hóstia viva ” Santa Regra II
+                  </em>
+                </p>
+                <p></p>
+              </div>
+            </div>
+            <Divider />
+
+            <div class="row mb-3">
+              <div class="col-5 d-flex justify-content-end">
+                <figure class="my-4">
+                  <img
+                    src={PreyImage5}
+                    alt=""
+                    width="350px"
+                    height="350px"
+                    class="img-fluid"
+                  />
+                </figure>
+              </div>
+              <div class="col-7" justify-content="left">
+                <h4>
+                  <Divider>No Coração da Igreja: Somos Intercessoras</Divider>
+                </h4>
                 <ShowMoreText
                   lines={15}
                   more={"Mostrar mais"}
@@ -271,6 +293,8 @@ const Prey = () => {
                 </ShowMoreText>
               </div>
             </div>
+            <Divider />
+
             <div>
               <p class="container" align="right">
                 <em>
@@ -284,8 +308,7 @@ const Prey = () => {
             </div>
           </div>
         </div>
-        <div class="col-3">
-        </div>
+        <div class="col-3"></div>
       </div>
     </>
   );
